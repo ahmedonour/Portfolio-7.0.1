ea716d0f618216f0080f6afa8aabfbefe1d1e6bc
Ahmed Onour <ahmedonour.49@gmail.com>
2023-03-31 16:08:27 +0200

    Adding todo app

--- src/routes/work/todo/+page.svelte
+++ src/routes/work/todo/+page.svelte
@@ -5,16 +5,7 @@
     import UniText from "../../../components/uni-text.svelte";
     import Tools from "../../../components/projects/tools.svelte";
     let image = [
-        "/onourai/img1.png",
-        "/onourai/img2.png",
-        "/onourai/img3.png",
-        "/onourai/img4.png",
-        "/onourai/img5.png",
-        "/onourai/img6.png",
-        "/onourai/img7.png",
-        "/onourai/img8.png",
-        "/onourai/img9.png",
-        "/onourai/img10.png",
+        "/img1.png",
     ]
 
 </script>
@@ -28,19 +19,19 @@
             <!-- svelte-ignore a11y-missing-attribute -->
             <ImageSlider images={image}/>
             <UniText 
-            myH1="OnourAi"
-            myP=" OnourAi is an Arabic AI Image Generator that utilizes the SvelteKit framework, the OpenAI API, and a translating API to generate images based on user inputs. The goal of this project is to provide an easy-to-use tool for users to create customized images in the Arabic language, even if they are not fluent in Arabic themselves. The AI algorithms will generate images based on user-provided input and translate the generated text into English for the final output."
+            myH1="ToDo App"
+            myP="The ToDo App is a web application that allows users to create, manage, and organize their tasks. The app is built using the SvelteKit framework, which provides an easy-to-use and lightweight platform for building single-page applications."
             />
             <div class="links">
-                <a href="https://github.com/ahmedonour/Image-Generator-SvelteKit" target="_blank"><i class="fab fa-github"/></a>
-                <a href="https://onodevai.netlify.app/" target="_blank"><i class="fas fa-globe"/></a>
+                <a href="https://github.com/ahmedonour/my-todo-app" target="_blank"><i class="fab fa-github"/></a>
+                <a href="https://onodev-todo-app.netlify.app/" target="_blank"><i class="fas fa-globe"/></a>
             </div>
             <h1>Tools</h1>
             <div class="tools">
                 <Tools toolName='Figma' />
                 <Tools toolName='SvelteKit' />
-                <Tools toolName='OpenAI' />
-                <Tools toolName='TranslateAI' />
+                <Tools toolName='Svelte' />
+                <Tools toolName='Javascript' />
             </div>
         </div>
     </section>
